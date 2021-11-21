b36421d3fa11dbbf69bb8915ea20c323a8a15336
Crafter98 <ghkdrn4321@naver.com>
2021-11-22 00:13:26 +0900

    폴더 변경

--- web/mainTop5.php
+++ web/mainTop5.php
@@ -1,36 +1,41 @@
 <?php
 
-// DB 연결 파트
-// $conn = mysqli_connect(
-//     '127.0.0.1',
-//     'root',
-//     'twailight7',
-//     'capstone'
-// );
+error_reporting(E_ALL);
 
+ini_set("display_errors", 1);
+
+// DB 연결 파트
 $conn = mysqli_connect(
-    '125.187.32.134',
-    'user',
-    'KAU',
+    '127.0.0.1',
+    'root',
+    'twailight7',
     'capstone'
 );
 
+// $conn = mysqli_connect(
+//     '125.187.32.134',
+//     'user',
+//     'KAU',
+//     'capstone'
+// );
+
 // 웹에서 선택된 date와 section 값 가져와서 query 작성
 $date = '\''.$_POST['date'].'\'';
-$section = '\''.$_POST['section'].'\'';
 
-$query = "SELECT keyword, frequency FROM primary_keywords WHERE date = ";
+$query = "SELECT keyword, frequency, section FROM primary_keywords WHERE date = ";
 $query .= $date;
-$query .= "and section =";
-$query .= $section;
 $query .= "ORDER BY frequency desc;";
 $result = mysqli_query($conn, $query);
 
-////////////////////// new code ////////////////////////
+// query 결과를 table로 만들어 웹에 return
+// $output = '';
+$output = array();
+
 $i = 1;
 while($row = mysqli_fetch_array($result)){
     // $output["keyword".$i] = $row["keyword"]."&#13;".$row["frequency"];
     $output["keyword".$i] = $row["keyword"];
+    $output["keyword".$i." section"] = $row["section"];
 
     $i = $i + 1;
     if($i > 5){
